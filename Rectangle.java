42a23f6eda82872776b267ff1064bba66f57aaa1
Mmeli Ntshakala <135122360+MmeliNtshakala@users.noreply.github.com>
2024-12-24 23:25:49 +0200

    rectangle area

---
+++ Rectangle.java
@@ -0,0 +1,8 @@
+public class Rectangle {
+  public static void main(String[] arguments) {
+    float length = 5.0f;
+    float breath = 6.0f;
+    float area = length * breath;
+    System.out.println(area);
+  }
+}
